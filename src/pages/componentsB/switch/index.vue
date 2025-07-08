e7eeac79b3382a4130277b59927d2bf3f0e20207
anyup <anyupxing@163.com>
2025-07-08 20:05:21 +0800

    refactor(colorful-uni-plus): 重构 u-switch 组件

--- src/pages/componentsB/switch/index.vue
+++ src/pages/componentsB/switch/index.vue
@@ -3,7 +3,7 @@
 		<view class="u-demo-wrap">
 			<view class="u-demo-title">演示效果</view>
 			<view class="u-demo-area">
-				<u-switch v-model="checked" :loading="loading" 
+				<u-switch v-model:value="checked" :loading="loading" 
 					:size="size" @change="change" 
 					:active-color="activeColor"
 					:disabled="disabled"
@@ -45,6 +45,7 @@
 </template>
 
 <script>
+import { $u } from '@/uni_modules/colorful-uni-plus';
 	export default {
 		data() {
 			return {
@@ -62,7 +63,7 @@
 			},
 			colorChange(index) {
 				let color = index == 0 ? 'primary' : index == 1 ? 'error' : index == 2 ? 'warning' : 'success';
-				this.activeColor = this.$u.color[color];
+				this.activeColor = $u.color[color];
 			},
 			sizeChange(index) {
 				this.size = index == 0 ? '40' : index == 1 ? '60' : '80';
@@ -75,11 +76,11 @@
 			},
 			asyncChange(index) {
 				if(this.checked && index == 1) {
-					this.$u.toast('请先关闭选择器');
+					$u.toast('请先关闭选择器');
 					return;
 				}
 				if(!this.checked && index == 0) {
-					this.$u.toast('请先打开选择器');
+					$u.toast('请先打开选择器');
 					return;
 				}
 				let str = index == 0 ? '是否要关闭？' : '是否要打开？';
