194202a2e8ee06cec9b70822789a896a3b2c9c7d
anyup <anyupxing@163.com>
2025-07-08 16:57:14 +0800

    refactor(citySelect): 更新城市选择器数据文件路径
    - 将城市选择器的数据文件路径从 province.js、city.js、area.js 修改为 province.ts、city.ts、area.ts

--- src/pages/template/citySelect/u-city-select.vue
+++ src/pages/template/citySelect/u-city-select.vue
@@ -47,9 +47,9 @@
 </template>
 
 <script>
-	import provinces from "uview-ui/libs/util/province.js";
-	import citys from "uview-ui/libs/util/city.js";
-	import areas from "uview-ui/libs/util/area.js";
+	import provinces from "uview-ui/libs/util/province.ts";
+	import citys from "uview-ui/libs/util/city.ts";
+	import areas from "uview-ui/libs/util/area.ts";
 	/**
 	 * city-select 省市区级联选择器
 	 * @property {String Number} z-index 弹出时的z-index值（默认1075）
