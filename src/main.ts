8cf46adde563d588324a5c8a673442c6edd9292e
anyup <anyupxing@163.com>
2025-07-11 17:19:00 +0800

    refactor(i18n): 重构国际化配置并简化代码

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,15 @@
-import { createSSRApp } from "vue";
-import App from "./App.vue";
-import { createI18n } from 'vue-i18n'
+import { createSSRApp } from 'vue';
+import App from './App.vue';
 
 import colorfulUniPlus from '@/uni_modules/colorful-uni-plus';
 
-
-// 引入语言包，注意路径
-import zhCN from '@/common/locales/zh.js';
-import enUS from '@/common/locales/en.js';
-
-const messages = {
-	'zh-CN': {
-    ...zhCN
-  },
-  'en-US': {
-    ...enUS
-  }
-}
-
-let i18nConfig = {
-  locale: uni.getStorageSync('currentLang') || 'zh-CN',
-  fallbackLocale: 'zh-CN',
-  messages,
-  legacy: false
-}
-
-const i18n = createI18n(i18nConfig)
+import i18n from '@/locales';
 
 export function createApp() {
-  const app = createSSRApp(App);
-  app.use(i18n);
-  app.use(colorfulUniPlus);
-  app.config.globalProperties.$t = i18n.global.t
-  return {
-    app,
-  };
+    const app = createSSRApp(App);
+    app.use(i18n);
+    app.use(colorfulUniPlus);
+    return {
+        app
+    };
 }
