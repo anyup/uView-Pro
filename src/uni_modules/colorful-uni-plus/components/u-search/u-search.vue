e404a74416806d927758c752ec61057aa01fb01a
anyup <anyupxing@163.com>
2025-07-16 14:44:09 +0800

    refactor(u-search): 重构搜索组件

--- src/uni_modules/colorful-uni-plus/components/u-search/u-search.vue
+++ src/uni_modules/colorful-uni-plus/components/u-search/u-search.vue
@@ -1,70 +1,56 @@
 <template>
-  <view
-    class="u-search"
-    @tap="clickHandler"
-    :style="{
-      margin: margin
-    }"
-  >
     <view
-      class="u-content"
-      :style="{
-        backgroundColor: bgColor,
-        borderRadius: shape == 'round' ? '100rpx' : '10rpx',
-        border: borderStyle,
-        height: height + 'rpx'
-      }"
+        class="u-search"
+        @tap="clickHandler"
+        :style="{
+            margin: margin
+        }"
     >
-      <view class="u-icon-wrap">
-        <u-icon
-          class="u-clear-icon"
-          :size="30"
-          :name="searchIcon"
-          :color="searchIconColor ? searchIconColor : color"
-        ></u-icon>
-      </view>
-      <input
-        confirm-type="search"
-        @blur="blur"
-        :value="modelValue"
-        @confirm="search"
-        @input="inputChange"
-        :disabled="disabled"
-        @focus="getFocus"
-        :focus="focus"
-        :maxlength="maxlength"
-        placeholder-class="u-placeholder-class"
-        :placeholder="placeholder"
-        :placeholder-style="`color: ${placeholderColor}`"
-        class="u-input"
-        type="text"
-        :style="[
-          {
-            textAlign: inputAlign,
-            color: color,
-            backgroundColor: bgColor
-          },
-          inputStyle
-        ]"
-      />
-      <view class="u-close-wrap" v-if="keyword && clearabled && focused" @tap="clear">
-        <u-icon class="u-clear-icon" name="close-circle-fill" size="34" color="#c0c4cc"></u-icon>
-      </view>
+        <view
+            class="u-content"
+            :style="{
+                backgroundColor: bgColor,
+                borderRadius: shape == 'round' ? '100rpx' : '10rpx',
+                border: borderStyle,
+                height: height + 'rpx'
+            }"
+        >
+            <view class="u-icon-wrap">
+                <u-icon class="u-clear-icon" :size="30" :name="searchIcon" :color="searchIconColor ? searchIconColor : color"></u-icon>
+            </view>
+            <input
+                confirm-type="search"
+                @blur="blur"
+                :value="modelValue"
+                @confirm="search"
+                @input="inputChange"
+                :disabled="disabled"
+                @focus="getFocus"
+                :focus="focus"
+                :maxlength="maxlength"
+                placeholder-class="u-placeholder-class"
+                :placeholder="placeholder"
+                :placeholder-style="`color: ${placeholderColor}`"
+                class="u-input"
+                type="text"
+                :style="[
+                    {
+                        textAlign: inputAlign,
+                        color: color,
+                        backgroundColor: bgColor
+                    },
+                    inputStyle
+                ]"
+            />
+            <view class="u-close-wrap" v-if="keyword && clearabled && focused" @tap="clear">
+                <u-icon class="u-clear-icon" name="close-circle-fill" size="34" color="#c0c4cc"></u-icon>
+            </view>
+        </view>
+        <view :style="[actionStyle]" class="u-action" :class="[showActionBtn || show ? 'u-action-active' : '']" @tap.stop.prevent="custom">{{ actionText }}</view>
     </view>
-    <view
-      :style="[actionStyle]"
-      class="u-action"
-      :class="[showActionBtn || show ? 'u-action-active' : '']"
-      @tap.stop.prevent="custom"
-      >{{ actionText }}</view
-    >
-  </view>
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch, nextTick } from 'vue'
-// 若有 $u 工具方法，按需引入：import { $u } from '../../';
-
 /**
  * search 搜索框
  * @description 搜索组件，集成了常见搜索框所需功能，用户可以一键引入，开箱即用。
@@ -97,154 +83,159 @@ import { ref, computed, watch, nextTick } from 'vue'
  * @event {Function} clear 用户点击清除按钮时触发
  * @example <u-search placeholder="日照香炉生紫烟" v-model="keyword"></u-search>
  */
-const props = defineProps({
-  /** 搜索框形状，round-圆形，square-方形 */
-  shape: { type: String, default: 'round' },
-  /** 搜索框背景色，默认值#f2f2f2 */
-  bgColor: { type: String, default: '#f2f2f2' },
-  /** 占位提示文字 */
-  placeholder: { type: String, default: '请输入关键字' },
-  /** 是否启用清除控件 */
-  clearabled: { type: Boolean, default: true },
-  /** 是否自动聚焦 */
-  focus: { type: Boolean, default: false },
-  /** 是否在搜索框右侧显示取消按钮 */
-  showAction: { type: Boolean, default: true },
-  /** 右边控件的样式 */
-  actionStyle: { type: Object, default: () => ({}) },
-  /** 取消按钮文字 */
-  actionText: { type: String, default: '搜索' },
-  /** 输入框内容对齐方式，可选值为 left|center|right */
-  inputAlign: { type: String, default: 'left' },
-  /** 是否启用输入框 */
-  disabled: { type: Boolean, default: false },
-  /** 开启showAction时，是否在input获取焦点时才显示 */
-  animation: { type: Boolean, default: false },
-  /** 边框颜色，只要配置了颜色，才会有边框 */
-  borderColor: { type: String, default: 'none' },
-  /** 输入框的初始化内容 */
-  modelValue: { type: String, default: '' },
-  /** 搜索框高度，单位rpx */
-  height: { type: [Number, String], default: 64 },
-  /** input输入框的样式，可以定义文字颜色，大小等，对象形式 */
-  inputStyle: { type: Object, default: () => ({}) },
-  /** 输入框最大能输入的长度，-1为不限制长度(来自uniapp文档) */
-  maxlength: { type: [Number, String], default: '-1' },
-  /** 搜索图标的颜色，默认同输入框字体颜色 */
-  searchIconColor: { type: String, default: '' },
-  /** 输入框字体颜色 */
-  color: { type: String, default: '#606266' },
-  /** placeholder的颜色 */
-  placeholderColor: { type: String, default: '#909399' },
-  /** 组件与其他上下左右元素之间的距离，带单位的字符串形式，如"30rpx"、"30rpx 20rpx"等写法 */
-  margin: { type: String, default: '0' },
-  /** 左边输入框的图标，可以为uView图标名称或图片路径 */
-  searchIcon: { type: String, default: 'search' }
-})
 
-const emit = defineEmits(['update:modelValue', 'change', 'search', 'custom', 'clear', 'focus', 'blur', 'click'])
+import { ref, computed, watch, nextTick } from 'vue';
+
+defineOptions({ name: 'u-search' });
+
+const props = defineProps({
+    /** 搜索框形状，round-圆形，square-方形 */
+    shape: { type: String, default: 'round' },
+    /** 搜索框背景色，默认值#f2f2f2 */
+    bgColor: { type: String, default: '#f2f2f2' },
+    /** 占位提示文字 */
+    placeholder: { type: String, default: '请输入关键字' },
+    /** 是否启用清除控件 */
+    clearabled: { type: Boolean, default: true },
+    /** 是否自动聚焦 */
+    focus: { type: Boolean, default: false },
+    /** 是否在搜索框右侧显示取消按钮 */
+    showAction: { type: Boolean, default: true },
+    /** 右边控件的样式 */
+    actionStyle: { type: Object, default: () => ({}) },
+    /** 取消按钮文字 */
+    actionText: { type: String, default: '搜索' },
+    /** 输入框内容对齐方式，可选值为 left|center|right */
+    inputAlign: { type: String, default: 'left' },
+    /** 是否启用输入框 */
+    disabled: { type: Boolean, default: false },
+    /** 开启showAction时，是否在input获取焦点时才显示 */
+    animation: { type: Boolean, default: false },
+    /** 边框颜色，只要配置了颜色，才会有边框 */
+    borderColor: { type: String, default: 'none' },
+    /** 输入框的初始化内容 */
+    modelValue: { type: String, default: '' },
+    /** 搜索框高度，单位rpx */
+    height: { type: [Number, String], default: 64 },
+    /** input输入框的样式，可以定义文字颜色，大小等，对象形式 */
+    inputStyle: { type: Object, default: () => ({}) },
+    /** 输入框最大能输入的长度，-1为不限制长度(来自uniapp文档) */
+    maxlength: { type: [Number, String], default: '-1' },
+    /** 搜索图标的颜色，默认同输入框字体颜色 */
+    searchIconColor: { type: String, default: '' },
+    /** 输入框字体颜色 */
+    color: { type: String, default: '#606266' },
+    /** placeholder的颜色 */
+    placeholderColor: { type: String, default: '#909399' },
+    /** 组件与其他上下左右元素之间的距离，带单位的字符串形式，如"30rpx"、"30rpx 20rpx"等写法 */
+    margin: { type: String, default: '0' },
+    /** 左边输入框的图标，可以为uView图标名称或图片路径 */
+    searchIcon: { type: String, default: 'search' }
+});
+
+const emit = defineEmits(['update:modelValue', 'change', 'search', 'custom', 'clear', 'focus', 'blur', 'click']);
 
 // 绑定输入框的值
-const keyword = ref(props.modelValue)
+const keyword = ref(props.modelValue);
 // 是否显示右边的清除图标
-const showClear = ref(false)
+const showClear = ref(false);
 // 控制右侧动画展开
-const show = ref(false)
+const show = ref(false);
 // 标记input当前状态是否处于聚焦中，如果是，才会显示右侧的清除控件
-const focused = ref(props.focus)
+const focused = ref(props.focus);
 
 // 监听v-model和props.value变化
 watch(
-  () => props.modelValue,
-  nVal => {
-    keyword.value = nVal
-  }
-)
+    () => props.modelValue,
+    nVal => {
+        keyword.value = nVal;
+    }
+);
 watch(keyword, nVal => {
-  emit('update:modelValue', nVal)
-  emit('change', nVal)
-})
+    emit('update:modelValue', nVal);
+    emit('change', nVal);
+});
 
 /**
  * 是否显示右侧action按钮
  */
 const showActionBtn = computed(() => {
-  if (!props.animation && props.showAction) return true
-  else return false
-})
+    if (!props.animation && props.showAction) return true;
+    else return false;
+});
 /**
  * 边框样式
  */
 const borderStyle = computed(() => {
-  if (props.borderColor) return `1px solid ${props.borderColor}`
-  else return 'none'
-})
+    if (props.borderColor) return `1px solid ${props.borderColor}`;
+    else return 'none';
+});
 
 /**
  * 监听input事件获取输入框内容的变化
  */
 function inputChange(e: any) {
-  keyword.value = e.detail.value
+    keyword.value = e.detail.value;
 }
 /**
  * 清空输入
  * 也可以作为用户通过ref形式调用清空输入框内容
  */
 function clear() {
-  keyword.value = ''
-  // 延后发出事件，避免在父组件监听clear事件时，value为更新前的值(不为空)
-  nextTick(() => {
-    emit('clear')
-  })
+    keyword.value = '';
+    // 延后发出事件，避免在父组件监听clear事件时，value为更新前的值(不为空)
+    nextTick(() => {
+        emit('clear');
+    });
 }
 /**
  * 确定搜索
  */
 function search(e: any) {
-  emit('search', e.detail.value)
-  try {
-    // #ifdef H5 || MP
-    uni.hideKeyboard()
-    // #endif
-  } catch (e) {}
+    emit('search', e.detail.value);
+    try {
+        // #ifdef H5 || MP
+        uni.hideKeyboard();
+        // #endif
+    } catch (e) {}
 }
 /**
  * 点击右边自定义按钮的事件
  */
 function custom() {
-  emit('custom', keyword.value)
-  try {
-    // #ifdef H5 || MP
-    uni.hideKeyboard()
-    // #endif
-  } catch (e) {}
+    emit('custom', keyword.value);
+    try {
+        // #ifdef H5 || MP
+        uni.hideKeyboard();
+        // #endif
+    } catch (e) {}
 }
 /**
  * 获取焦点
  */
 function getFocus() {
-  focused.value = true
-  // 开启右侧搜索按钮展开的动画效果
-  if (props.animation && props.showAction) show.value = true
-  emit('focus', keyword.value)
+    focused.value = true;
+    // 开启右侧搜索按钮展开的动画效果
+    if (props.animation && props.showAction) show.value = true;
+    emit('focus', keyword.value);
 }
 /**
  * 失去焦点
  */
 function blur() {
-  // 最开始使用的是监听图标@touchstart事件，自从hx2.8.4后，此方法在微信小程序出错
-  // 这里改为监听点击事件，手点击清除图标时，同时也发生了@blur事件，导致图标消失而无法点击，这里做一个延时
-  setTimeout(() => {
-    focused.value = false
-  }, 100)
-  show.value = false
-  emit('blur', keyword.value)
+    // 最开始使用的是监听图标@touchstart事件，自从hx2.8.4后，此方法在微信小程序出错
+    // 这里改为监听点击事件，手点击清除图标时，同时也发生了@blur事件，导致图标消失而无法点击，这里做一个延时
+    setTimeout(() => {
+        focused.value = false;
+    }, 100);
+    show.value = false;
+    emit('blur', keyword.value);
 }
 /**
  * 点击搜索框，只有disabled=true时才发出事件，因为禁止了输入，意味着是想跳转真正的搜索页
  */
 function clickHandler() {
-  if (props.disabled) emit('click')
+    if (props.disabled) emit('click');
 }
 </script>
 
@@ -252,56 +243,56 @@ function clickHandler() {
 @import '../../libs/css/style.components.scss';
 
 .u-search {
-  @include vue-flex;
-  align-items: center;
-  flex: 1;
+    @include vue-flex;
+    align-items: center;
+    flex: 1;
 }
 
 .u-content {
-  @include vue-flex;
-  align-items: center;
-  padding: 0 18rpx;
-  flex: 1;
+    @include vue-flex;
+    align-items: center;
+    padding: 0 18rpx;
+    flex: 1;
 }
 
 .u-clear-icon {
-  @include vue-flex;
-  align-items: center;
+    @include vue-flex;
+    align-items: center;
 }
 
 .u-input {
-  flex: 1;
-  font-size: 28rpx;
-  line-height: 1;
-  margin: 0 10rpx;
-  color: $u-tips-color;
+    flex: 1;
+    font-size: 28rpx;
+    line-height: 1;
+    margin: 0 10rpx;
+    color: $u-tips-color;
 }
 
 .u-close-wrap {
-  width: 40rpx;
-  height: 100%;
-  @include vue-flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
+    width: 40rpx;
+    height: 100%;
+    @include vue-flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
 }
 
 .u-placeholder-class {
-  color: $u-tips-color;
+    color: $u-tips-color;
 }
 
 .u-action {
-  font-size: 28rpx;
-  color: $u-main-color;
-  width: 0;
-  overflow: hidden;
-  transition: all 0.3s;
-  white-space: nowrap;
-  text-align: center;
+    font-size: 28rpx;
+    color: $u-main-color;
+    width: 0;
+    overflow: hidden;
+    transition: all 0.3s;
+    white-space: nowrap;
+    text-align: center;
 }
 
 .u-action-active {
-  width: 80rpx;
-  margin-left: 10rpx;
+    width: 80rpx;
+    margin-left: 10rpx;
 }
 </style>
