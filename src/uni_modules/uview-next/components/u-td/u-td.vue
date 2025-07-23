efcf0ccc33439168918d9e7f89a45a7123bb2f4f
anyup <anyupxing@163.com>
2025-07-23 14:35:48 +0800

    refactor(table): 重构表格组件并添加 TypeScript 支持
    - 重构 u-table、u-td、u-th 和 u-tr 组件

--- src/uni_modules/uview-next/components/u-td/u-td.vue
+++ src/uni_modules/uview-next/components/u-td/u-td.vue
@@ -1,66 +1,75 @@
 <template>
-	<view class="u-td" :style="[tdStyle]">
-		<slot></slot>
-	</view>
+    <view class="u-td" :style="tdStyle">
+        <slot></slot>
+    </view>
 </template>
 
-<script>
-	/**
-	 * td td单元格
-	 * @description 表格组件一般用于展示大量结构化数据的场景（搭配u-table使用）
-	 * @tutorial https://www.uviewui.com/components/table.html#td-props
-	 * @property {String Number} width 单元格宽度百分比或者具体带单位的值，如30%， 200rpx等，一般使用百分比，单元格宽度默认为均分tr的长度（默认auto）
-	 * @example <u-td>二年级</u-td>
-	 */
-	export default {
-		name: "u-td",
-		props: {
-			// 宽度，百分比或者具体带单位的值，如30%， 200rpx等，一般使用百分比
-			width: {
-				type: [Number, String],
-				default: 'auto'
-			}
-		},
-		data() {
-			return {
-				tdStyle: {
-					
-				}
-			}
-		},
-		created() {
-			this.parent = false;
-		},
-		mounted() {
-			this.parent = this.$u.$parent.call(this, 'u-table');
-			if (this.parent) {
-				// 将父组件的相关参数，合并到本组件
-				let style = {};
-				if (this.width != "auto") style.flex = `0 0 ${this.width}`;
-				style.textAlign = this.parent.align;
-				style.fontSize = this.parent.fontSize + 'rpx';
-				style.padding = this.parent.padding;
-				style.borderBottom = `solid 1px ${this.parent.borderColor}`;
-				style.borderRight = `solid 1px ${this.parent.borderColor}`;
-				style.color = this.parent.color;
-				this.tdStyle = style;
-			}
-		}
-	};
+<script setup lang="ts">
+/**
+ * td td单元格
+ * @description 表格组件一般用于展示大量结构化数据的场景（搭配u-table使用）
+ * @tutorial https://www.uviewui.com/components/table.html#td-props
+ * @property {String | Number} width 单元格宽度百分比或者具体带单位的值，如30%， 200rpx等，一般使用百分比，单元格宽度默认为均分tr的长度（默认auto）
+ * @example <u-td>二年级</u-td>
+ */
+defineOptions({ name: 'u-td' });
+
+import { ref, onMounted, getCurrentInstance } from 'vue';
+import { $u } from '../../';
+
+const props = defineProps({
+    /**
+     * 宽度，百分比或者具体带单位的值，如30%， 200rpx等，一般使用百分比
+     */
+    width: {
+        type: [Number, String],
+        default: 'auto'
+    }
+});
+
+/**
+ * 组合式API变量声明
+ * 保留所有说明注释
+ */
+const tdStyle = ref<Record<string, any>>({}); // 单元格样式
+let parent: any = null; // 父组件实例
+
+/**
+ * 组件挂载时查找父组件u-table并合并样式
+ */
+onMounted(() => {
+    // 查找父组件u-table
+    const instance = getCurrentInstance();
+    if (instance) {
+        parent = $u.$parent('u-table');
+        if (parent) {
+            // 将父组件的相关参数，合并到本组件
+            const style: Record<string, any> = {};
+            if (props.width !== 'auto') style.flex = `0 0 ${props.width}`;
+            style.textAlign = parent.props.align;
+            style.fontSize = parent.props.fontSize + 'rpx';
+            style.padding = parent.props.padding;
+            style.borderBottom = `solid 1px ${parent.props.borderColor}`;
+            style.borderRight = `solid 1px ${parent.props.borderColor}`;
+            style.color = parent.props.color;
+            tdStyle.value = style;
+        }
+    }
+});
 </script>
 
 <style lang="scss" scoped>
-	@import "../../libs/css/style.components.scss";
+@import '../../libs/css/style.components.scss';
 
-	.u-td {
-		@include vue-flex;
-		flex-direction: column;
-		flex: 1;
-		justify-content: center;
-		font-size: 28rpx;
-		color: $u-content-color;
-		align-self: stretch;
-		box-sizing: border-box;
-		height: 100%;
-	}
+.u-td {
+    @include vue-flex;
+    flex-direction: column;
+    flex: 1;
+    justify-content: center;
+    font-size: 28rpx;
+    color: $u-content-color;
+    align-self: stretch;
+    box-sizing: border-box;
+    height: 100%;
+}
 </style>
--- src/uni_modules/uview-next/components/u-th/u-th.vue
+++ src/uni_modules/uview-next/components/u-th/u-th.vue
@@ -1,62 +1,69 @@
 <template>
-	<view class="u-th" :style="[thStyle]">
-		<slot></slot>
-	</view>
+    <view class="u-th" :style="thStyle">
+        <slot></slot>
+    </view>
 </template>
 
-<script>
-	/**
-	 * th th单元格
-	 * @description 表格组件一般用于展示大量结构化数据的场景（搭配u-table使用）
-	 * @tutorial https://www.uviewui.com/components/table.html#td-props
-	 * @property {String Number} width 标题单元格宽度百分比或者具体带单位的值，如30%，200rpx等，一般使用百分比，单元格宽度默认为均分tr的长度
-	 * @example 暂无示例
-	 */
-	export default {
-		name: "u-th",
-		props: {
-			// 宽度，百分比或者具体带单位的值，如30%， 200rpx等，一般使用百分比
-			width: {
-				type: [Number, String],
-				default: ''
-			}
-		},
-		data() {
-			return {
-				thStyle: {}
-			}
-		},
-		created() {
-			this.parent = false;
-		},
-		mounted() {
-			this.parent = this.$u.$parent.call(this, 'u-table');
-			if (this.parent) {
-				// 将父组件的相关参数，合并到本组件
-				let style = {};
-				if (this.width) style.flex = `0 0 ${this.width}`;
-				style.textAlign = this.parent.align;
-				style.padding = this.parent.padding;
-				style.borderBottom = `solid 1px ${this.parent.borderColor}`;
-				style.borderRight = `solid 1px ${this.parent.borderColor}`;
-				Object.assign(style, this.parent.thStyle);
-				this.thStyle = style;
-			}
-		}
-	};
+<script setup lang="ts">
+/**
+ * th th单元格
+ * @description 表格组件一般用于展示大量结构化数据的场景（搭配u-table使用）
+ * @tutorial https://www.uviewui.com/components/table.html#td-props
+ * @property {String | Number} width 标题单元格宽度百分比或者具体带单位的值，如30%，200rpx等，一般使用百分比，单元格宽度默认为均分tr的长度
+ * @example 暂无示例
+ */
+defineOptions({ name: 'u-th' });
+
+import { ref, onMounted, getCurrentInstance } from 'vue';
+import { $u } from '../../';
+
+const props = defineProps({
+    /**
+     * 宽度，百分比或者具体带单位的值，如30%， 200rpx等，一般使用百分比
+     */
+    width: {
+        type: [Number, String],
+        default: ''
+    }
+});
+
+const thStyle = ref<Record<string, any>>({}); // 标题单元格样式
+let parent: any = null; // 父组件实例
+
+/**
+ * 组件挂载时查找父组件u-table并合并样式
+ */
+onMounted(() => {
+    // 查找父组件u-table
+    const instance = getCurrentInstance();
+    if (instance) {
+        parent = $u.$parent('u-table');
+        if (parent) {
+            // 将父组件的相关参数，合并到本组件
+            const style: Record<string, any> = {};
+            if (props.width) style.flex = `0 0 ${props.width}`;
+            style.textAlign = parent.props.align;
+            style.padding = parent.props.padding;
+            style.borderBottom = `solid 1px ${parent.props.borderColor}`;
+            style.borderRight = `solid 1px ${parent.props.borderColor}`;
+            Object.assign(style, parent.props.thStyle);
+            thStyle.value = style;
+        }
+    }
+});
 </script>
 
 <style lang="scss" scoped>
-	@import "../../libs/css/style.components.scss";
+@import '../../libs/css/style.components.scss';
 
-	.u-th {
-		@include vue-flex;
-		flex-direction: column;
-		flex: 1;
-		justify-content: center;
-		font-size: 28rpx;
-		color: $u-main-color;
-		font-weight: bold;
-		background-color: rgb(245, 246, 248);
-	}
+.u-th {
+    @include vue-flex;
+    flex-direction: column;
+    flex: 1;
+    justify-content: center;
+    font-size: 28rpx;
+    color: $u-main-color;
+    font-weight: bold;
+    background-color: rgb(245, 246, 248);
+}
 </style>
