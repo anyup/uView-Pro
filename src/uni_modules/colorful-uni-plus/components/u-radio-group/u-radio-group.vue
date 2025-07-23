526c62f5c41d1a24dc15d7de986b57bf725d86c7
anyup <anyupxing@163.com>
2025-07-23 11:51:42 +0800

    refactor(radio): 重构单选框组件

--- src/uni_modules/colorful-uni-plus/components/u-radio-group/u-radio-group.vue
+++ src/uni_modules/colorful-uni-plus/components/u-radio-group/u-radio-group.vue
@@ -1,128 +1,116 @@
 <template>
-	<view class="u-radio-group u-clearfix">
-		<slot></slot>
-	</view>
+    <view class="u-radio-group u-clearfix">
+        <slot></slot>
+    </view>
 </template>
 
-<script>
-	import Emitter from '../../libs/util/emitter.js';
-	/**
-	 * radioGroup 单选框父组件
-	 * @description 单选框用于有一个选择，用户只能选择其中一个的场景。搭配u-radio使用
-	 * @tutorial https://www.uviewui.com/components/radio.html
-	 * @property {Boolean} disabled 是否禁用所有radio（默认false）
-	 * @property {String Number} size 组件整体的大小，单位rpx（默认40）
-	 * @property {String} active-color 选中时的颜色，应用到所有子Radio组件（默认#2979ff）
-	 * @property {String Number} icon-size 图标大小，单位rpx（默认20）
-	 * @property {String} shape 外观形状，shape-方形，circle-圆形(默认circle)
-	 * @property {Boolean} label-disabled 是否禁止点击文本操作checkbox(默认false)
-	 * @property {String} width 宽度，需带单位
-	 * @property {Boolean} wrap 是否每个radio都换行（默认false）
-	 * @event {Function} change 任一个radio状态发生变化时触发
-	 * @example <u-radio-group v-model="value"></u-radio-group>
-	 */
-	export default {
-		name: "u-radio-group",
-		mixins: [Emitter],
-		props: {
-			// 是否禁用所有单选框
-			disabled: {
-				type: Boolean,
-				default: false
-			},
-			// 匹配某一个radio组件，如果某个radio的name值等于此值，那么这个radio就被会选中
-			value: {
-				type: [String, Number],
-				default: ''
-			},
-			// 选中状态下的颜色
-			activeColor: {
-				type: String,
-				default: '#2979ff'
-			},
-			// 组件的整体大小
-			size: {
-				type: [String, Number],
-				default: 34
-			},
-			// 是否禁止点击提示语选中复选框
-			labelDisabled: {
-				type: Boolean,
-				default: false
-			},
-			// 形状，square为方形，circle为原型
-			shape: {
-				type: String,
-				default: 'circle'
-			},
-			// 图标的大小，单位rpx
-			iconSize: {
-				type: [String, Number],
-				default: 20
-			},
-			// 每个checkbox占u-checkbox-group的宽度
-			width: {
-				type: [String, Number],
-				default: 'auto'
-			},
-			// 是否每个checkbox都换行
-			wrap: {
-				type: Boolean,
-				default: false
-			}
-		},
-		created() {
-			// 如果将children定义在data中，在微信小程序会造成循环引用而报错
-			this.children = [];
-		},
-		watch: {
-			// 当父组件需要子组件需要共享的参数发生了变化，手动通知子组件
-			parentData() {
-				if(this.children.length) {
-					this.children.map(child => {
-						// 判断子组件(u-radio)如果有updateParentData方法的话，就就执行(执行的结果是子组件重新从父组件拉取了最新的值)
-						typeof(child.updateParentData) == 'function' && child.updateParentData();
-					})
-				}
-			},
-		},
-		computed: {
-			// 这里computed的变量，都是子组件u-radio需要用到的，由于头条小程序的兼容性差异，子组件无法实时监听父组件参数的变化
-			// 所以需要手动通知子组件，这里返回一个parentData变量，供watch监听，在其中去通知每一个子组件重新从父组件(u-radio-group)
-			// 拉取父组件新的变化后的参数
-			parentData() {
-				return [this.value, this.disabled, this.activeColor, this.size, this.labelDisabled, this.shape, this.iconSize, this.width, this.wrap];
-			}
-		},
-		methods: {
-			// 该方法有子组件radio调用，当一个radio被选中的时候，给父组件设置value值(props传递的value)
-			setValue(val) {
-				// 通过子组件传递过来的val值(此被选中的子组件内部已将parentValue设置等于val的值)，将其他
-				// u-radio设置未选中的状态
-				this.children.map(child => {
-					if(child.parentData.value != val) child.parentData.value = '';
-				})
-				// 通过emit事件，设置父组件通过v-model双向绑定的值
-				this.$emit('input', val);
-				this.$emit('change', val);
-				// 等待下一个周期再执行，因为this.$emit('input')作用于父组件，再反馈到子组件内部，需要时间
-				// 由于头条小程序执行迟钝，故需要用几十毫秒的延时
-				setTimeout(() => {
-					// 将当前的值发送到 u-form-item 进行校验
-					this.dispatch('u-form-item', 'on-form-change', val);
-				}, 60)
-			}
-		}
-	}
+<script setup lang="ts">
+import { ref, provide, watch, getCurrentInstance } from 'vue';
+import { $u } from '../../';
+
+/**
+ * radioGroup 单选框父组件
+ * @description 单选框用于有一个选择，用户只能选择其中一个的场景。搭配u-radio使用
+ * @tutorial https://www.uviewui.com/components/radio.html
+ * @property {Boolean} disabled 是否禁用所有radio（默认false）
+ * @property {String|Number} size 组件整体的大小，单位rpx（默认40）
+ * @property {String} active-color 选中时的颜色，应用到所有子Radio组件（默认#2979ff）
+ * @property {String|Number} icon-size 图标大小，单位rpx（默认20）
+ * @property {String} shape 外观形状，shape-方形，circle-圆形(默认circle)
+ * @property {Boolean} label-disabled 是否禁止点击文本操作checkbox(默认false)
+ * @property {String|Number} width 宽度，需带单位
+ * @property {Boolean} wrap 是否每个radio都换行（默认false）
+ * @event {Function} change 任一个radio状态发生变化时触发
+ * @example <u-radio-group v-model="value"></u-radio-group>
+ */
+
+defineOptions({
+    name: 'u-radio-group'
+});
+
+const props = defineProps({
+    /** 是否禁用所有单选框 */
+    disabled: { type: Boolean, default: false },
+    /** 匹配某一个radio组件，如果某个radio的name值等于此值，那么这个radio就被会选中 */
+    modelValue: { type: [String, Number], default: '' },
+    /** 选中状态下的颜色 */
+    activeColor: { type: String, default: '#2979ff' },
+    /** 组件的整体大小 */
+    size: { type: [String, Number], default: 34 },
+    /** 是否禁止点击提示语选中复选框 */
+    labelDisabled: { type: Boolean, default: false },
+    /** 形状，square为方形，circle为原型 */
+    shape: { type: String, default: 'circle' },
+    /** 图标的大小，单位rpx */
+    iconSize: { type: [String, Number], default: 20 },
+    /** 每个checkbox占u-checkbox-group的宽度 */
+    width: { type: [String, Number], default: 'auto' },
+    /** 是否每个checkbox都换行 */
+    wrap: { type: Boolean, default: false }
+});
+
+const emit = defineEmits(['update:modelValue', 'change']);
+
+// 当前所有子 radio 实例
+const children = ref<any[]>([]);
+
+/**
+ * 父组件数据，供子组件 inject 使用
+ */
+function getData() {
+    return {
+        iconSize: props.iconSize,
+        labelDisabled: props.labelDisabled,
+        disabled: props.disabled,
+        shape: props.shape,
+        activeColor: props.activeColor,
+        size: props.size,
+        width: props.width,
+        wrap: props.wrap,
+        value: props.modelValue
+    };
+}
+
+/**
+ * 设置选中值
+ * @param val 选中的 radio 的 name
+ */
+function setValue(val: string | number) {
+    // 通过emit事件，设置父组件通过v-model双向绑定的值
+    emit('update:modelValue', val);
+    emit('change', val);
+    // 等待下一个周期再执行，因为emit作用于父组件，再反馈到子组件内部，需要时间
+    // 由于头条小程序执行迟钝，故需要用几十毫秒的延时
+    setTimeout(() => {
+        // 将当前的值发送到 u-form-item 进行校验
+        $u.dispatch(getCurrentInstance(), 'u-form-item', 'on-form-change', val);
+    }, 60);
+}
+
+// provide 父组件数据和方法，供子组件使用
+provide('u-radio-group', {
+    getData,
+    setValue
+});
+
+// 监听 props 变化，通知子组件刷新
+watch(
+    () => [props.modelValue, props.disabled, props.activeColor, props.size, props.labelDisabled, props.shape, props.iconSize, props.width, props.wrap],
+    () => {
+        // 这里可通过事件或响应式通知子组件刷新
+        // 兼容性处理，子组件可通过 inject 的 getData 获取最新值
+        console.log('u-radio-group 监听到属性变化');
+    }
+);
 </script>
 
 <style lang="scss" scoped>
-	@import "../../libs/css/style.components.scss";
-	
-	.u-radio-group {
-		/* #ifndef MP || APP-NVUE */
-		display: inline-flex;
-		flex-wrap: wrap;
-		/* #endif */
-	}
+@import '../../libs/css/style.components.scss';
+
+.u-radio-group {
+    /* #ifndef MP || APP-NVUE */
+    display: inline-flex;
+    flex-wrap: wrap;
+    /* #endif */
+}
 </style>
