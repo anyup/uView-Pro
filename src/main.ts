f4bcc4dd976784ce885eddf56795808428f9c16e
anyup <anyupxing@163.com>
2025-08-04 18:35:28 +0800

    refactor(src): 更新 uview-pro 插件引用

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { createSSRApp } from 'vue';
 import App from './App.vue';
 
-import colorfulUniPlus from '@/uni_modules/uview-pro';
+import uViewPro from '@/uni_modules/uview-pro';
 
 import i18n from '@/locales';
 
 export function createApp() {
     const app = createSSRApp(App);
     app.use(i18n);
-    app.use(colorfulUniPlus);
+    app.use(uViewPro);
     return {
         app
     };
