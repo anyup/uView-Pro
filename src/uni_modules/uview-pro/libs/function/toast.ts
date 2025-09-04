22320540acee36c6c11688387431a4ddba93520f
anyup <anyupxing@163.com>
2025-09-04 13:18:47 +0800

    feat: 增强 toast 工具函数的灵活性

--- src/uni_modules/uview-pro/libs/function/toast.ts
+++ src/uni_modules/uview-pro/libs/function/toast.ts
@@ -1,13 +1,13 @@
 /**
  * 显示无图标的 Toast 提示
  * @param title 提示文本
- * @param duration 显示时长（毫秒），默认1500
+ * @param option 显示时长（毫秒）默认1500 /显示图标，默认为none，
  */
-function toast(title: string, duration: number = 1500): void {
+function toast(title: string, option: number | string | Record<string, any> = 1500): void {
     uni.showToast({
         title: title,
-        icon: 'none',
-        duration: duration
+        icon: typeof option === 'string' ? option : typeof option === 'object' ? option.icon || 'none' : 'none',
+        duration: typeof option === 'number' ? option : typeof option === 'object' ? option.duration || '1500' : 1500
     });
 }
 
