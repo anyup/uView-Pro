b3192882a104963ab6a2c7b6c03e2fb8ee17f609
anyup <anyupxing@163.com>
2025-09-19 07:34:28 +0800

    build(vite): 配置别名和服务器设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [uni()]
-    // resolve: {
-    //   alias: {
-    //     "@": path.join(process.cwd(), "./src"),
-    //     "@img": path.join(process.cwd(), "./src/static/images"),
-    //   },
-    // },
-    // server: {
-    //   port: 6000,
-    //   fs: {
-    //     // Allow serving files from one level up to the project root
-    //     allow: [".."],
-    //   },
-    // },
+    plugins: [uni()],
+    resolve: {
+        alias: {
+            '@': path.join(process.cwd(), './src'),
+            'uview-pro': path.join(process.cwd(), './src/uni_modules/uview-pro')
+        }
+    },
+    server: {
+        port: 5173,
+        fs: {
+            // Allow serving files from one level up to the project root
+            allow: ['..']
+        }
+    }
 });
