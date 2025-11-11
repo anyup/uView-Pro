3c24a75de00982bbdc1388a413721377de4f1e3d
anyup <anyupxing@163.com>
2025-11-11 18:22:31 +0800

    fix(u-root-portal): 修复根节点传送组件运行时报错问题

--- src/uni_modules/uview-pro/components/u-root-portal/u-root-portal.vue
+++ src/uni_modules/uview-pro/components/u-root-portal/u-root-portal.vue
@@ -18,6 +18,12 @@
     <!-- #endif -->
 </template>
 <script>
+/**
+ * rootPortal 根节点传送
+ * @description 该组件一般用于使整个子树从页面中脱离出来，类似于在 CSS 中使用 fixed position 的效果。主要用于制作弹窗、弹出层等。
+ * @tutorial https://uviewpro.cn/zh/components/rootPortal.html
+ * @example <u-root-portal><view class="modal">这是一个全局弹窗</view></u-root-portal>
+ */
 export default {
     name: 'u-root-portal',
     options: {
@@ -29,14 +35,7 @@ export default {
     }
 };
 </script>
-<script lang="ts" setup>
-/**
- * rootPortal 根节点传送
- * @description 该组件一般用于使整个子树从页面中脱离出来，类似于在 CSS 中使用 fixed position 的效果。主要用于制作弹窗、弹出层等。
- * @tutorial https://uviewpro.cn/zh/components/rootPortal.html
- * @example <u-root-portal><view class="modal">这是一个全局弹窗</view></u-root-portal>
- */
-</script>
+<script lang="ts" setup></script>
 
 <!-- #ifdef APP-PLUS -->
 <script module="render" lang="renderjs">
