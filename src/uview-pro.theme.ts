cab8be38832bca9de1254a06f199e6b3704ee88e
anyup <anyupxing@163.com>
2025-11-29 19:38:06 +0800

    refactor: 更新自定义主题文件

--- src/uview-pro.theme.ts
+++ src/uview-pro.theme.ts
@@ -1,47 +1,7 @@
-// 此自定义uView Pro主题生成于2025-11-05 13:18
-// 地址：https://uviewpro.cn/zh/guide/theme.html
+// 此自定义uView Pro主题生成于2025-11-29 13:18
+// 地址：https://uviewpro.cn/zh/guide/themeGenerate.html
 
-import type { Theme, ThemeColor } from './uni_modules/uview-pro/types/global';
-
-export const theme: ThemeColor = {
-    primary: '#19be6b',
-    primaryDark: '#2b85e4',
-    primaryDisabled: '#a0cfff',
-    primaryLight: '#ecf5ff',
-    success: '#19be6b',
-    successDark: '#18b566',
-    successDisabled: '#71d5a1',
-    successLight: '#dbf1e1',
-    warning: '#ff9900',
-    warningDark: '#f29100',
-    warningDisabled: '#fcbd71',
-    warningLight: '#fdf6ec',
-    error: '#fa3534',
-    errorDark: '#dd6161',
-    errorDisabled: '#fab6b6',
-    errorLight: '#fef0f0',
-    info: '#909399',
-    infoDark: '#82848a',
-    infoDisabled: '#c8c9cc',
-    infoLight: '#f4f4f5',
-    whiteColor: '#ffffff',
-    blackColor: '#000000',
-    mainColor: '#303133',
-    contentColor: '#606266',
-    tipsColor: '#909399',
-    lightColor: '#c0c4cc',
-    borderColor: '#e4e7ed',
-    dividerColor: '#e4e7ed',
-    maskColor: 'rgba(0, 0, 0, 0.4)',
-    shadowColor: 'rgba(0, 0, 0, 0.1)',
-    bgColor: '#f3f4f6',
-    bgWhite: '#ffffff',
-    bgGrayLight: '#f5f7fa',
-    bgGrayDark: '#2f343c',
-    bgBlack: '#000000'
-};
-
-export const themes: Theme[] = [
+export default [
     // 主题 1: 紫色
     {
         name: 'purple',
