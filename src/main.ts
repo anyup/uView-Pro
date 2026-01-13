e4f8bf5baae237e8dc443de7c90cd751ef045101
anyup <anyupxing@163.com>
2026-01-13 11:52:22 +0800

    feat(mp-weixin): 添加小程序全局分享功能

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import themes from '@/uview-pro.theme';
 import uViewPro, { httpPlugin } from '@/uni_modules/uview-pro';
 import { httpInterceptor, httpRequestConfig } from './common/http.interceptor';
 import i18n, { getDefaultLocale } from '@/locales';
-
+// #ifdef MP
+import share from './common/share';
+// #endif
 export function createApp() {
     const app = createSSRApp(App);
     app.use(i18n);
@@ -22,6 +24,9 @@ export function createApp() {
     });
     // 引入uView Pro 的http插件
     app.use(httpPlugin, { interceptor: httpInterceptor, requestConfig: httpRequestConfig });
+    // #ifdef MP
+    app.mixin(share);
+    // #endif
     return {
         app
     };
