b00751b2643bd582f2d1b9472e245d856352d4ed
anyup <anyupxing@163.com>
2026-02-09 10:48:37 +0800

    chore(deps): 更新 uni-app 相关依赖包版本到 4.87，修复更新包后运行到 h5 报错问题

--- vite.config.ts
+++ vite.config.ts
@@ -13,16 +13,6 @@ export default defineConfig({
             'uview-pro/': path.join(process.cwd(), './src/uni_modules/uview-pro/')
         }
     },
-    define: {
-        __VUE_I18N_FULL_INSTALL__: true,
-        __VUE_I18N_LEGACY_API__: false,
-        __INTLIFY_PROD_DEVTOOLS__: false,
-        __VUE_OPTIONS_API__: true,
-        __VUE_PROD_DEVTOOLS__: false
-    },
-    esbuild: {
-        target: 'es2015'
-    },
     optimizeDeps: {
         exclude: ['vue-i18n']
     },
