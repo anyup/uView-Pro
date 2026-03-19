cd6a2ad234920d6c1a0a4e6fe32032af3f48cdaa
刘欢 <liuhuan@cnic.cn>
2026-03-19 16:53:01 +0800

    fix(u-swiper): 优化u-swiper组件，增加插槽，可以自定义swiper组件的内容

--- src/uni_modules/uview-pro/components/u-swiper/u-swiper.vue
+++ src/uni_modules/uview-pro/components/u-swiper/u-swiper.vue
@@ -27,14 +27,16 @@
                         margin: effect3d && uCurrent != index ? '0 20rpx' : 0
                     }"
                 >
-                    <image class="u-swiper-image" :src="item[name] || item" :mode="imgMode"></image>
-                    <view
-                        v-if="title && item.title"
-                        class="u-swiper-title u-line-1"
-                        :style="[{ 'padding-bottom': titlePaddingBottom }, titleStyle]"
-                    >
-                        {{ item.title }}
-                    </view>
+                    <slot name="item" :item="item" :index="index" :isActive="uCurrent === index">
+                        <image class="u-swiper-image" :src="item[name] || item" :mode="imgMode"></image>
+                        <view
+                            v-if="title && item.title"
+                            class="u-swiper-title u-line-1"
+                            :style="[{ 'padding-bottom': titlePaddingBottom }, titleStyle]"
+                        >
+                            {{ item.title }}
+                        </view>
+                    </slot>
                 </view>
             </swiper-item>
         </swiper>
