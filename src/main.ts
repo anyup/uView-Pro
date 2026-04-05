4caa2edf9542f7f2b0a7bb9870585390f0241e80
anyup <anyupxing@163.com>
2026-04-05 22:31:43 +0800

    feat(theme): 更新主题配置和本地化文本

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,10 @@ export function createApp() {
     // 引入uView Pro 主库
     app.use(uViewPro, {
         theme: {
-            themes: themes,
-            defaultTheme: 'green',
-            defaultDarkMode: 'auto'
+            themes: themes, // 主题对象数组，至少需要包含一个主题对象，且每个对象必须包含 name 和 color 字段
+            defaultTheme: 'green', // 默认主题名称，需与 themes 中的 name 字段对应
+            defaultDarkMode: 'auto', // 默认暗黑模式跟随系统，支持 'auto' | 'light' | 'dark'
+            isForce: false // 是否初始化强制使用默认主题，如果为true，则会覆盖用户之前选择的主题；如果为false，则会优先使用用户之前选择的主题
         },
         locale: {
             // 部分覆盖内置语言包
@@ -23,7 +24,7 @@ export function createApp() {
                 { name: 'zh-CN', uModal: { confirmText: '好的', cancelText: '算了' } },
                 { name: 'en-US', uModal: { confirmText: 'OK', cancelText: 'Cancel' } }
             ],
-            defaultLocale: 'zh-CN'
+            defaultLocale: 'zh-CN' // 默认语言环境，需与 locales 中的 name 字段对应
         },
         debugMode: false // 是否开启日志和警告
     });
