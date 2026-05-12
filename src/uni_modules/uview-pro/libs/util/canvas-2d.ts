05ebe2998007421cf2d2db8664bbe2963419129f
anyup <anyupxing@163.com>
2026-05-12 12:38:25 +0800

    fix(canvas): 修复头条小程序中CanvasContext的Illegal invocation错误

--- src/uni_modules/uview-pro/libs/util/canvas-2d.ts
+++ src/uni_modules/uview-pro/libs/util/canvas-2d.ts
@@ -4,7 +4,46 @@
  * @returns
  */
 export function canvas2d(ctx: CanvasRenderingContext2D): UniApp.CanvasContext {
-    return Object.assign(ctx, {
+    // 绑定 this 到 ctx，避免头条小程序等环境中出现 Illegal invocation 错误
+    const boundCtx = {
+        arc: ctx.arc.bind(ctx),
+        arcTo: ctx.arcTo.bind(ctx),
+        beginPath: ctx.beginPath.bind(ctx),
+        bezierCurveTo: ctx.bezierCurveTo.bind(ctx),
+        clearRect: ctx.clearRect.bind(ctx),
+        clip: ctx.clip.bind(ctx),
+        closePath: ctx.closePath.bind(ctx),
+        createImageData: ctx.createImageData.bind(ctx),
+        createLinearGradient: ctx.createLinearGradient.bind(ctx),
+        createPattern: ctx.createPattern.bind(ctx),
+        createRadialGradient: ctx.createRadialGradient.bind(ctx),
+        drawImage: ctx.drawImage.bind(ctx),
+        fill: ctx.fill.bind(ctx),
+        fillRect: ctx.fillRect.bind(ctx),
+        fillText: ctx.fillText.bind(ctx),
+        getImageData: ctx.getImageData.bind(ctx),
+        getLineDash: ctx.getLineDash.bind(ctx),
+        isPointInPath: ctx.isPointInPath.bind(ctx),
+        isPointInStroke: ctx.isPointInStroke.bind(ctx),
+        lineTo: ctx.lineTo.bind(ctx),
+        measureText: ctx.measureText.bind(ctx),
+        moveTo: ctx.moveTo.bind(ctx),
+        putImageData: ctx.putImageData.bind(ctx),
+        quadraticCurveTo: ctx.quadraticCurveTo.bind(ctx),
+        rect: ctx.rect.bind(ctx),
+        restore: ctx.restore.bind(ctx),
+        rotate: ctx.rotate.bind(ctx),
+        save: ctx.save.bind(ctx),
+        scale: ctx.scale.bind(ctx),
+        setLineDash: ctx.setLineDash.bind(ctx),
+        setTransform: ctx.setTransform.bind(ctx),
+        stroke: ctx.stroke.bind(ctx),
+        strokeRect: ctx.strokeRect.bind(ctx),
+        strokeText: ctx.strokeText.bind(ctx),
+        transform: ctx.transform.bind(ctx),
+        translate: ctx.translate.bind(ctx),
+        resetTransform: ctx.resetTransform?.bind(ctx),
+        roundRect: (ctx as any).roundRect?.bind(ctx),
         setFillStyle(color: string | CanvasGradient) {
             ctx.fillStyle = color;
         },
@@ -45,5 +84,99 @@ export function canvas2d(ctx: CanvasRenderingContext2D): UniApp.CanvasContext {
         createCircularGradient() {},
         draw() {},
         addColorStop() {}
-    }) as unknown as UniApp.CanvasContext;
+    };
+    // 复制属性访问器
+    Object.defineProperties(boundCtx, {
+        fillStyle: {
+            get: () => ctx.fillStyle,
+            set: value => {
+                ctx.fillStyle = value;
+            }
+        },
+        strokeStyle: {
+            get: () => ctx.strokeStyle,
+            set: value => {
+                ctx.strokeStyle = value;
+            }
+        },
+        lineWidth: {
+            get: () => ctx.lineWidth,
+            set: value => {
+                ctx.lineWidth = value;
+            }
+        },
+        lineCap: {
+            get: () => ctx.lineCap,
+            set: value => {
+                ctx.lineCap = value;
+            }
+        },
+        lineJoin: {
+            get: () => ctx.lineJoin,
+            set: value => {
+                ctx.lineJoin = value;
+            }
+        },
+        miterLimit: {
+            get: () => ctx.miterLimit,
+            set: value => {
+                ctx.miterLimit = value;
+            }
+        },
+        globalAlpha: {
+            get: () => ctx.globalAlpha,
+            set: value => {
+                ctx.globalAlpha = value;
+            }
+        },
+        globalCompositeOperation: {
+            get: () => ctx.globalCompositeOperation,
+            set: value => {
+                ctx.globalCompositeOperation = value;
+            }
+        },
+        font: {
+            get: () => ctx.font,
+            set: value => {
+                ctx.font = value;
+            }
+        },
+        textAlign: {
+            get: () => ctx.textAlign,
+            set: value => {
+                ctx.textAlign = value;
+            }
+        },
+        textBaseline: {
+            get: () => ctx.textBaseline,
+            set: value => {
+                ctx.textBaseline = value;
+            }
+        },
+        shadowOffsetX: {
+            get: () => ctx.shadowOffsetX,
+            set: value => {
+                ctx.shadowOffsetX = value;
+            }
+        },
+        shadowOffsetY: {
+            get: () => ctx.shadowOffsetY,
+            set: value => {
+                ctx.shadowOffsetY = value;
+            }
+        },
+        shadowBlur: {
+            get: () => ctx.shadowBlur,
+            set: value => {
+                ctx.shadowBlur = value;
+            }
+        },
+        shadowColor: {
+            get: () => ctx.shadowColor,
+            set: value => {
+                ctx.shadowColor = value;
+            }
+        }
+    });
+    return boundCtx as unknown as UniApp.CanvasContext;
 }
