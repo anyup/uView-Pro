f540d45fc5bf1ba340138699872d8f97857656fb
anyup <anyupxing@163.com>
2026-07-29 23:31:22 +0800

    refactor(types): 优化uview-pro的类型声明和导出配置

--- src/uni_modules/uview-pro/types/index.d.ts
+++ src/uni_modules/uview-pro/types/index.d.ts
@@ -3,19 +3,12 @@
 
 export * from './global';
 
-import { $u } from '../libs';
-
-// uview-pro 模块类型声明
-declare module 'uview-pro' {
-    // 导出安装函数
-    export function install(): void;
-}
-
-// 全局类型扩展
 declare global {
     interface Uni {
-        $u: typeof $u;
+        $u: typeof import('../libs').$u;
     }
 }
 
-export {};
+declare module 'uview-pro' {
+    export function install(): void;
+}
